6512163e583d4a872227bb134be59008ca29074e
Nnaemekac <nnaemekachimezie5@gmail.com>
2025-02-18 01:16:22 -0800

    ---

--- src/components/about/CoreValues.jsx
+++ src/components/about/CoreValues.jsx
@@ -31,6 +31,19 @@ const CoreValues = () => {
                     <p className='xui-font-sz-95 xui-mt-half xui-font-w-500'>Natural & Organic Products</p>
                 </div>
             </div>
+            <div className='xui-d-grid xui-lg-grid-col-2 xui-grid-col-1 xui-mt-3'>
+                <div>
+                    <h3 className='xui-font-sz-150 xui-font-w-500'>// Core Values</h3>
+                </div>
+                <div>
+                    <ul className='xui-p-none'>
+                        <li className='xui-font-sz-85 xui-lg-w-fluid-90 xui-w-fluid-100 xui-line-height-1-half'><span className='xui-font-w-600'>Quality:</span> We prioritize offering only the best products from trusted and renowned brands.</li>
+                        <li className='xui-font-sz-85 xui-lg-w-fluid-90 xui-w-fluid-100 xui-line-height-1-half xui-mt-1'><span className='xui-font-w-600'>Diversity:</span> We embrace global beauty trends, featuring a mix of designer, niche, and Arabian products.</li>
+                        <li className='xui-font-sz-85 xui-lg-w-fluid-90 xui-w-fluid-100 xui-line-height-1-half xui-mt-1'><span className='xui-font-w-600'>Accessibility:</span> We aim to make luxury beauty accessible to every customer.</li>
+                        <li className='xui-font-sz-85 xui-lg-w-fluid-90 xui-w-fluid-100 xui-line-height-1-half xui-mt-1'><span className='xui-font-w-600'>Innovation:</span> Constantly evolving to meet the needs of our customers with cutting-edge beauty solutions.</li>
+                    </ul>
+                </div>
+            </div>
         </section>
     );
 };
